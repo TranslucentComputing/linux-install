8bfe38a3e7963582c60307dface0800825115535
Patryk Golabek <pgolabek@gmail.com>
2016-06-01 14:37:50 -0400

    gcloud updates

--- centos-install.sh
+++ centos-install.sh
@@ -1,29 +1,36 @@
 echo "========TC-INC CentOS Bootup=============="
 
-echo "======================"
+echo "===================================="
 echo "install DOCKER... yo!!!"
-echo "======================"
+echo "===================================="
 sudo curl -fsSL https://raw.githubusercontent.com/TranslucentComputing/docker-install/master/main-centos.sh | bash
 
-echo "======================"
+echo "===================================="
 echo "install GIT... yo!!!"
-echo "======================"
+echo "===================================="
 sudo yum install git -y
 
-
-echo "======================"
+echo "===================================="
 echo "install gcloud... yo!!!"
-echo "======================"
-sudo curl https://sdk.cloud.google.com | bash
+echo "===================================="
+sudo yum install wget -y
+sudo wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
+sudo google-cloud-sdk/install.sh --usage-reporting=true --path-update=true --bash-completion=true --rc-path=/.bashrc
+# Disable updater check for the whole installation.
+# Users won't be bugged with notifications to update to the latest version of gcloud.
+sudo google-cloud-sdk/bin/gcloud config set --installation component_manager/disable_update_check true
 
 
-echo "======================"
+echo "===================================="
 echo "install ahoy!!!"
-echo "======================"
-yum install wget -y
+echo "===================================="
 sudo wget -q https://github.com/devinci-code/ahoy/releases/download/1.1.0/ahoy-`uname -s`-amd64 -O /usr/local/bin/ahoy && sudo chown $USER /usr/local/bin/ahoy && chmod +x /usr/local/bin/ahoy
 
+echo "===================================="
+echo "refreshing shell"
+echo "===================================="
+source ~/.bash_profile
 
-echo "======================"
+echo "===================================="
 echo "Server is up and ready"
-echo "======================"
+echo "===================================="
